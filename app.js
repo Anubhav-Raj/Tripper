1f26fb907256c70afa364ccf7127a64eac8b913a
Anubhav Rajput <72142278+Anubhav-Raj@users.noreply.github.com>
2023-04-07 22:53:08 +0530

    add comman folder and allproductpage

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-import connectDB from './db/db.js';
-import express from 'express';
-import bodyParser from 'body-parser';
+import connectDB from "./db/db.js";
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+
 connectDB();
 const PORT = 5005;
 
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.render('landing');
+  res.render("landing");
+});
+app.get("/product", (req, res) => {
+  res.render("allproduct");
 });
 
 app.listen(PORT, () => {
