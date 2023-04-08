2613f2798cb39ec9bde9a0e8d542c173aeb4d7d1
Anubhav Rajput <72142278+Anubhav-Raj@users.noreply.github.com>
2023-04-08 16:37:44 +0530

    package blog  added

--- app.js
+++ app.js
@@ -13,11 +13,13 @@ const PORT = 5005;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(session({
-  secret: 'mugiwara',
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: "mugiwara",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(passport.initialize());
@@ -29,24 +31,38 @@ const userSchema = new mongoose.Schema({
   googleId: String,
   facebookId: String,
   githubId: String,
-  secret: String
+  secret: String,
 });
 
 const User = new mongoose.model("User", userSchema);
 
-passport.use(new GitHubStrategy({
-  clientID: 'c191b26ca6beaa25b802',
-  clientSecret: '1583ec0c7dce732697b8124e2fa3ccd8e3a77823',
-  callbackURL: "http://127.0.0.1:3000/auth/github/allpackage"
-},
-function(accessToken, refreshToken, profile, done) {
-  console.log(profile.id);
-}
-));
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: "c191b26ca6beaa25b802",
+      clientSecret: "1583ec0c7dce732697b8124e2fa3ccd8e3a77823",
+      callbackURL: "http://127.0.0.1:3000/auth/github/allpackage",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      console.log(profile.id);
+    }
+  )
+);
 app.use("/profile", express.static("upload/images"));
 
 app.get("/", (req, res) => {
   res.render("Screen/landing", {
+    pageTitle: "Tripper",
+  });
+});
+
+app.get("/package_details", (req, res) => {
+  res.render("package_details", {
+    pageTitle: "Tripper",
+  });
+});
+app.get("/allblog", (req, res) => {
+  res.render("blog", {
     pageTitle: "Tripper",
   });
 });
@@ -63,36 +79,40 @@ app.get("/navigation", (req, res) => {
   });
 });
 
-app.get('/login', (req, res)=>{
-  res.render('login/login');
+app.get("/login", (req, res) => {
+  res.render("login/login");
 });
-app.get('/signup', (req, res)=>{
-  res.render('signup/signup');
+app.get("/signup", (req, res) => {
+  res.render("signup/signup");
 });
-app.get('/auth/github',
-  passport.authenticate('github', { scope: [ 'user:email' ] }));
+app.get(
+  "/auth/github",
+  passport.authenticate("github", { scope: ["user:email"] })
+);
 
-app.get('/auth/github/allpackage', 
-  passport.authenticate('github', { failureRedirect: '/login' }),
-  function(req, res) {
+app.get(
+  "/auth/github/allpackage",
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  function (req, res) {
     // Successful authentication, redirect home.
-    res.redirect('/');
-  });
+    res.redirect("/");
+  }
+);
 
-  app.post('/login', (req, res)=>{
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password
-    })
-    req.login(user, (err)=>{
-        if(err){
-            console.log(err);
-        }else{
-            passport.authenticate("local")(req, res, ()=>{
-                res.redirect("/allpackage");
-            })
-        }
-    })
+app.post("/login", (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/allpackage");
+      });
+    }
+  });
 });
 
 //   passport.use(User.createStrategy());
@@ -108,14 +128,12 @@ app.get('/auth/github/allpackage',
 //       });
 //     });
 //   });
-  
+
 //   passport.deserializeUser(function(user, cb) {
 //     process.nextTick(function() {
 //       return cb(null, user);
 //     });
 //   });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
