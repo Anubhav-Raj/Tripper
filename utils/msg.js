f5ceff4c34088c5574983531a7ce4568757de6a2
Shubham Kumar Singh <95537249+theAlpha06@users.noreply.github.com>
2023-04-09 08:23:28 +0530

    fixed mail and msg

--- utils/msg.js
+++ utils/msg.js
@@ -2,14 +2,16 @@ const twilio = require('twilio');
 require('dotenv').config()
 
 const accountSid = process.env.ACCOUNT_SID;
-const authToken = process.env.AUTH_TOKEN;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const client = require('twilio')(accountSid, authToken);
+
+// module.exports = client;
 
 client.messages
     .create({
         body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-        from: '+14406414438',
+        from: process.env.TWILIO_PHONE,
         to: '+919506846608'
     })
     .then(message => console.log(message.sid));
